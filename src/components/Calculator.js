04cab349d5f837e30e5a69acdfb8a1632dfb1bcd
newhorizon-tech <alaukikinbox@gmail.com>
2021-11-22 14:35:14 +0530

    Add calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Calculator extends Component {
+  constructor() {
+    super();
+    this.state = [];
+  }
+
+  render() {
+    return (<div> Calculator </div>);
+  }
+}
+
+export default Calculator;
