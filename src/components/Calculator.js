8a7e4c6877462333c59647e317c94324e68169d5
newhorizon-tech <alaukikinbox@gmail.com>
2021-11-22 20:46:21 +0530

    Add button list

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Row from './Row';
 
 class Calculator extends Component {
   constructor() {
@@ -7,7 +8,19 @@ class Calculator extends Component {
   }
 
   render() {
-    return (<div> Calculator </div>);
+    const buttonRows = [
+      ['AC', '+/-', '%', '÷'],
+      ['7', '8', '9', 'X'],
+      ['4', '5', '6', '-'],
+      ['1', '2', '3', '+'],
+      ['0', '.', '='],
+    ];
+    return (
+      <div>
+        <div> Calculator </div>
+        <div>{buttonRows.map((row) => <Row key={row[0]} row={row} />)}</div>
+      </div>
+    );
   }
 }
 
